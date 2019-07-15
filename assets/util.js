e5f7813dfa14a1d6f97317cb790306b0b18deb1d
Daniel Cañizares Corrales <dacanizares@outlook.com>
2019-07-14 20:52:47 -0500

    Improving table of contents
    Closes #17
    
    Co-Authored-By: Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>

--- assets/util.js
+++ assets/util.js
@@ -31,17 +31,62 @@ wikilaterus.UpdateActiveMenu = function (prevUrl, url) {
     $('#sideNavbar').removeClass('show');
 }
 
+wikilaterus.ParseHeading = function(element, state, htmlTOC) {
+    const elementType = element ? element.prop('nodeName').toLowerCase() : '';
+    console.log(state);
+    console.log(element);
+    switch (state) {
+        case '':
+            if (elementType === 'h2') {
+                // Open h2
+                htmlTOC += `<li><a href="#${element.prop('id')}">${element.html()}</a>`;
+                state = 'inside-h2';
+            }
+            break;
+        case 'inside-h2':
+            if (elementType === 'h2') {
+                // Close previous h2 and continue
+                htmlTOC += `</li><li><a href="#${element.prop('id')}">${element.html()}</a>`;
+                state = 'inside-h2';
+            } else if (elementType === 'h3') {
+                // Open an h3
+                htmlTOC += `<ul><li><a href="#${element.prop('id')}">${element.html()}</a></li>`;
+                state = 'inside-h3';
+            } else {
+                // Close h2
+                htmlTOC += `</li>`;
+                state = '';
+            }
+            break;
+        case 'inside-h3':
+            if (elementType === 'h2') {
+                // Close h3 and h2 and continue
+                htmlTOC += `</ul></li><li><a href="#${element.prop('id')}">${element.html()}</a>`;
+                state = 'inside-h2';
+            } else if (elementType === 'h3') {
+                // Continue
+                htmlTOC += `<li><a href="#${element.prop('id')}">${element.html()}</a></li>`;
+                state = 'inside-h3';
+            } else {
+                // Close h3 and h2
+                htmlTOC += `</ul></li>`;
+                state = '';
+            }
+            break;
+    }
+    
+    return [ htmlTOC, state ];
+}
+
 wikilaterus.GenerateTOC = function () {
     let htmlTOC = '<ol>';
-    let insideH2 = false;
+    let state = '';
     $('#main-content h2, #main-content h3').each(function() {
-        const elementType = $(this).prop('nodeName').toLowerCase();
-        
-        if (elementType == 'h2') {
-            htmlTOC += `<li><a href="#${$(this).prop('id')}">${$(this).html()}</a></li>`
-        }
+        [htmlTOC, state] = wikilaterus.ParseHeading($(this), state, htmlTOC);
     });
+    [htmlTOC, state] = wikilaterus.ParseHeading(null, state, htmlTOC);
     htmlTOC += '</ol>';
+    console.log(htmlTOC);
     return htmlTOC;
 }
 
